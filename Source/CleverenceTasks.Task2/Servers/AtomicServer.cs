7f4891128508f2a4ec1db7dacbb1fae753b95ef2
Andreev-Da <andreev.da@inbres.ru>
2026-08-11 15:07:59 +0300

    feat: добавлен AtomicServer
    AtomicServer решает проблему гонки потоков. На мой взгляд достаточное решение для смоделированной области, но недостаточное по условиям задачи

---
+++ Source/CleverenceTasks.Task2/Servers/AtomicServer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleverenceTasks.Task2.Servers
+{
+    public class AtomicServer(ILogger<SimpleServer> logger) : IServer
+    {
+        private volatile int _count;
+
+        public int GetCount()
+        {
+            return _count;
+        }
+
+        public void AddToCount(int value)
+        {
+            int attempts = 0;
+            int count, newValue;
+            do
+            {
+                attempts++;
+                count = _count;
+                newValue = _count + value;
+            } while (Interlocked.CompareExchange(ref _count, newValue, count) != count);
+
+            logger.LogDebug("Count changed {0} -> {1} ({2})", count, newValue, attempts);
+        }
+    }
+}
