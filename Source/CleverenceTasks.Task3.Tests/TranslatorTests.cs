b43df26e1923aa5470bda0f29142919e7666c188
Andreev-Da <andreev.da@inbres.ru>
2026-08-12 16:11:08 +0300

    feat: поддержан паттерн Disposing в тестах

--- Source/CleverenceTasks.Task3.Tests/TranslatorTests.cs
+++ Source/CleverenceTasks.Task3.Tests/TranslatorTests.cs
@@ -4,17 +4,16 @@ namespace CleverenceTasks.Task3.Tests;
 
 public class TranslatorTests
 {
-    private const int Increment = 1;
-
     [Test]
     [LogDataGenerator]
     public async Task ReadWriteTest(IReadOnlyCollection<Log> logs)
     {
         List<Log> output = [];
-        var writer = new CollectionLogWriter(output);
-        var reader = new CollectionLogReader(logs);
-
-        var translator = new Translator(reader, writer);
+        using var writer = new CollectionLogWriter(output);
+        using var reader = new CollectionLogReader(logs);
+        
+        using var translator = new Translator(reader, writer);
+        
         await translator.TranslateAsync();
 
         await Assert.That(output).IsEquivalentTo(logs);
