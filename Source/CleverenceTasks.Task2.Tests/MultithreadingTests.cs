8d5204cfe7cf6b149b6feb15b342673ac0c29255
Andreev-Da <andreev.da@inbres.ru>
2026-08-12 12:59:46 +0300

    feat: добавлены тесты читателей/писателей

--- Source/CleverenceTasks.Task2.Tests/MultithreadingTests.cs
+++ Source/CleverenceTasks.Task2.Tests/MultithreadingTests.cs
@@ -6,11 +6,17 @@ using ILoggerFactory = Microsoft.Extensions.Logging.ILoggerFactory;
 
 namespace CleverenceTasks.Task2.Tests;
 
+/// <summary>
+/// Тесты поведения в многопоточной среде. Словно лучше подошли бы какие то стрес тесты, либо что то более похожее на фаззинг. 
+/// Т.к. данные тесты не идемпотентны и могут давать разные результаты
+/// </summary>
 public class MultithreadingTests
 {
     private const int Increment = 1;
     
-    
+    /// <summary>
+    /// Тест 50 на 50, авось повезет и мы встретим гонку потоков
+    /// </summary>
     [Test]
     [CombinedDataSources]
     [NotInParallel]
@@ -21,9 +27,37 @@ public class MultithreadingTests
         IReadOnlyCollection<IClient> clients = new ClientFactory(server, new MockLoggerFactory(), Increment)
             .CreateMany(readersCount: 0, writersCount);
 
+        Run(clients);
+        
+        await Assert.That(server.GetCount()).IsEqualTo(writersCount * Increment);
+    }
+
+    /// <summary>
+    /// Обычный прогон, гипотетически можем выявить взаимоблокировки
+    /// </summary>
+    [Test]
+    [CombinedDataSources]
+    [NotInParallel]
+    public async Task RegularTest(
+        [ServersDataSource] IServer server,
+        [Arguments(8, 16, 32)] int readersCount,
+        [Arguments(8, 16, 32)] int writersCount
+    )
+    {
+        IReadOnlyCollection<IClient> clients = new ClientFactory(server, new MockLoggerFactory(), Increment)
+            .CreateMany(readersCount: readersCount, writersCount);
+
+        Run(clients);
+
+        await Assert.That(server.GetCount()).IsEqualTo(writersCount * Increment);
+    }
+
+
+    private void Run(IReadOnlyCollection<IClient> clients)
+    {
         List<Thread> threads = new List<Thread>(clients.Count);
         SemaphoreSlim launcher = new SemaphoreSlim(0, clients.Count);
-        
+
         // Не использовал Parallel, т.к. он вроде бы работает с ThreadPool, а хочется честную конкуренцию
         foreach (IClient client in clients)
         {
@@ -33,18 +67,16 @@ public class MultithreadingTests
                 Thread.Sleep(TimeSpan.FromSeconds(0.1));
                 client.DoWork();
             });
-            
+
             thread.Start();
             threads.Add(thread);
         }
-        
+
         launcher.Release(clients.Count);
 
         foreach (Thread thread in threads)
         {
             thread.Join();
         }
-        
-        await Assert.That(server.GetCount()).IsEqualTo(writersCount * Increment);
     }
 }
