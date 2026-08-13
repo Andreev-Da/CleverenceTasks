cf4fc18ab3092aafc2ea529126be4d849c58c663
Andreev-Da <andreev.da@inbres.ru>
2026-08-13 09:13:10 +0300

    feat: добавлены тесты парсинга для неверных форматов

---
+++ Source/CleverenceTasks.Task3.Tests/Mocks/LogsFormatingHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleverenceTasks.Task3.Tests.Mocks
+{
+    internal static class LogsFormatingHelper
+    {
+        public static string ToFormat1(IReadOnlyCollection<Log> logs)
+        {
+            return string.Join("\n", logs.Select(LogFormatter1));
+        }
+
+        public static string ToFormat2(IReadOnlyCollection<Log> logs)
+        {
+            return string.Join("\n", logs.Select(LogFormatter2));
+        }
+
+        private static string LogFormatter1(Log log)
+        {
+            //Формат 1: 10.03.2025 15:14:49.523 INFORMATION Версия программы: '3.4.0.48729'
+            //Дата: 10.03.2025
+            //Время: 15:14:49.523
+            //УровеньЛогирования: INFORMATION
+            //Сообщение: Версия программы: ‘3.4.0.48729’
+            var builder = new StringBuilder();
+
+            builder.AppendFormat("{0:dd.MM.yyyy} ", log.DateTime);
+            builder.AppendFormat("{0:HH:mm:ss.fff} ", log.DateTime);
+            builder.AppendFormat("{0} ", GetLogLevelString1(log.LogLevel));
+            builder.AppendFormat("{0}", log.Message);
+
+            return builder.ToString();
+        }
+
+        private static string GetLogLevelString1(LogLevel logLevel)
+        {
+            return logLevel switch
+            {
+                LogLevel.Debug => "DEBUG",
+                LogLevel.Warning => "WARNING",
+                LogLevel.Information => "INFORMATION",
+                LogLevel.Error => "ERROR"
+            };
+        }
+
+        private static string LogFormatter2(Log log)
+        {
+            //Формат 1: 10.03.2025 15:14:49.523 INFORMATION Версия программы: '3.4.0.48729'
+            //Дата: 10.03.2025
+            //Время: 15:14:49.523
+            //УровеньЛогирования: INFORMATION
+            //Сообщение: Версия программы: ‘3.4.0.48729’
+            var builder = new StringBuilder();
+
+            builder.AppendFormat("{0:yyyy-MM-dd HH:mm:ss.ffff}|", log.DateTime);
+            builder.AppendFormat(" {0}|", GetLogLevelString2(log.LogLevel));
+            builder.AppendFormat("{0}|", log.CallerMemberName);
+            builder.AppendFormat("{0}", log.Message);
+
+            return builder.ToString();
+        }
+
+        private static string GetLogLevelString2(LogLevel logLevel)
+        {
+            return logLevel switch
+            {
+                LogLevel.Debug => "DEBUG",
+                LogLevel.Warning => "WARNING",
+                LogLevel.Information => "INFORMATION",
+                LogLevel.Error => "ERROR"
+            };
+        }
+    }
+}
---
+++ Source/CleverenceTasks.Task3.Tests/ParsingTests.cs
@@ -0,0 +1,91 @@
+﻿using CleverenceTasks.Task3.Parsers;
+using CleverenceTasks.Task3.Readers;
+using CleverenceTasks.Task3.Tests.Mocks;
+using CleverenceTasks.Task3.Writers;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CleverenceTasks.Task3.Tests
+{
+    internal class ParsingTests
+    {
+        [Test]
+        [LogDataGenerator]
+        public async Task Formater1Test(IReadOnlyCollection<Log> logs)
+        {
+            var parser = new Format1Parser();
+
+            var result = await RunReader(parser, LogsFormatingHelper.ToFormat1(logs)).ToListAsync();
+
+            await Assert.That(logs.Count).IsEqualTo(result.Count);
+        }
+
+        [Test]
+        [LogDataGenerator]
+        public async Task Formater2Test(IReadOnlyCollection<Log> logs)
+        {
+            var parser = new Format2Parser();
+
+            var result = await RunReader(parser, LogsFormatingHelper.ToFormat2(logs)).ToListAsync();
+
+            await Assert.That(logs.Count).IsEqualTo(result.Count);
+        }
+
+
+        [Test]
+        [LogDataGenerator]
+        public async Task Formater1InvalidDataTest(IReadOnlyCollection<Log> logs)
+        {
+            if (logs.Count == 0)
+                return;
+
+            TestInvalidFormatAsync(
+                new Format1Parser(),
+                LogsFormatingHelper.ToFormat2(logs)
+            );
+        }
+
+        [Test]
+        [LogDataGenerator]
+        public async Task Formater2InvalidDataTest(IReadOnlyCollection<Log> logs)
+        {
+            if (logs.Count == 0)
+                return;
+
+            TestInvalidFormatAsync(
+                new Format2Parser(), 
+                LogsFormatingHelper.ToFormat1(logs)
+            );
+        }
+
+
+        private async IAsyncEnumerable<Log> RunReader(ILogParser parser, string value)
+        {
+            using var reader = new StreamLogReader(new StringReader(value), parser);
+
+            Log? log = await reader.ReadNextAsync();
+            while (log != null)
+            {
+                yield return log;
+                log = await reader.ReadNextAsync();
+            }
+        }
+
+        private async Task TestInvalidFormatAsync(ILogParser parser, string input)
+        {
+            try
+            {
+                var result = await RunReader(parser, input).ToListAsync();
+            }
+            catch (ParseException ex)
+            {
+                return;
+            }
+
+            Assert.Fail("Format parser accepts incorrect input data");
+        }
+    }
+}
